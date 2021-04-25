5c1d32f6e01df32b51f3e9fe8f455aee40a948a0
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-25 22:34:10 +0000

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+  
+  def forex_home
+    url = "https://api.exchangerate.host/symbols"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    symbols_hash = parsed_data.fetch("symbols")
+    @currency_symbols = symbols_hash.keys
+
+    render({:template => "forex_templates/forex_home.html.erb"})
+  end
+
+  def forex_base_currency
+    url = "https://api.exchangerate.host/symbols"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    symbols_hash = parsed_data.fetch("symbols")
+    @currency_symbols = symbols_hash.keys
+
+    render({:template => "forex_templates/forex_base_currency.html.erb"})
+  end
+
+  def calculate_forex_rate
+    url = "https://api.exchangerate.host/symbols"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    symbols_hash = parsed_data.fetch("symbols")
+    @currency_symbols = symbols_hash.keys    
+
+    render({:template => "forex_templates/forex_calculation.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/forex", {:controller => "application", :action => "forex_home"})
+  get("/forex/:first_currency", {:controller => "application", :action => "forex_base_currency"})
+  get("/forex/:first_currency/:second_currency", {:controller => "application", :action => "calculate_forex_rate"})
+  
 end
